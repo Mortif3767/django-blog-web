348ad04ea093ce282ef723865ea479a2846a8126
mortif3767 <mortif3767@outlook.com>
2017-09-25 22:28:05 +0800

    chap1

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,14 @@
 from django.db import models
 from django.utils import timezone             #django的时间工具
 from django.contrib.auth.models import User   #auth应用
+from django.core.urlresolvers import reverse
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,
+                     self).get_queryset().filter(status='published')
+
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -26,13 +34,12 @@ class Post(models.Model):
     class Meta:
         ordering = ('-publish',)  #查询返回顺序为publish列降序排列
 
+    def get_absolute_url(self):   #模型标准urls
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.strftime('%m'),
+                             self.publish.strftime('%d'),
+                             self.slug])
+
     def __str__(self):
         return self.title
-
-
-class PublishedManager(models.Manager):
-    def get_queryset(self):
-        return super(PublishedManager,
-                     self).get_queryset().filter(status='published')
-
-
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,24 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 from .models import Post
 
 
 def post_list(request):
-    posts = Post.published.all()
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)  #每页显示三个博客
+    page = request.GET.get('page')   #flask里为request.args.get('page'):GET请求传參
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
-                  {'posts': posts})
+                  {'posts': posts,
+                   'page': page})        #留意这个传过去的posts，为何在页码模版中可以posts.page?(页面对象)
 
 
 def post_detail(request, year, month, day, post):
@@ -18,3 +30,10 @@ def post_detail(request, year, month, day, post):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+
+class PostListView(ListView):         #django定义了view视图的基础类
+    queryset = Post.published.all()   #代替超类取回所有对象，默认django构建model.objects.all()
+    context_object_name = 'posts'     #查询结果赋给变量posts，否则默认object_list
+    paginate_by = 3                   #传给模板的页面对象为page_obj
+    template_name = 'blog/post/list.html'
