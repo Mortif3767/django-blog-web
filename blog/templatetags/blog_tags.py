e7df8b5b718246d92ecf6ff4ebe5a61d2df2018a
mortif3767 <mortif3767@outlook.com>
2017-09-28 17:19:56 +0800

    lalaa

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from django import template
+
+register = template.Library() #每个template_tag都要有的变量，用以表明有效标签库
+
+from django.db.models import Count
+from django.utils.safestring import mark_safe
+from ..models import Post
+import markdown
+
+
+@register.simple_tag #注册标签，并表明标签类型
+def total_posts():
+    return Post.published.count()
+
+
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
+
+
+@register.assignment_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(
+        total_comments=Count('comments')).order_by('-total_comments')[:count]
+
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
