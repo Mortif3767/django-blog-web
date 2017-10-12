1dd488075e5a6af099b52714998d3f0e1ef7b2e5
mortif3767 <mortif3767@outlook.com>
2017-10-12 17:15:50 +0800

    return

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -9,18 +9,18 @@ from ..models import Post
 import markdown
 
 
-@register.simple_tag #注册标签，并表明标签类型
+@register.simple_tag #注册标签，并表明标签类型；simpletag直接返回结果
 def total_posts():
     return Post.published.count()
 
 
-@register.inclusion_tag('blog/post/latest_posts.html')
+@register.inclusion_tag('blog/post/latest_posts.html') #包含标签必须返回一个字典值
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by('-publish')[:count]
     return {'latest_posts': latest_posts}
 
 
-@register.assignment_tag
+@register.assignment_tag   #将结果返回在变量中，用法{% template_tag as variable(变量) %}
 def get_most_commented_posts(count=5):
     return Post.published.annotate(
         total_comments=Count('comments')).order_by('-total_comments')[:count]
